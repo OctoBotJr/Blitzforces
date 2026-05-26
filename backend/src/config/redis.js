1edc12b92c6e38b81cf8fa3ea34c8d1ea9e9d9a9
Sanjeev Prasanna <vvsanjeevprasanna@gmail.com>
2026-05-26 17:01:56 +0530

    fix: rediss TLS url for upstash

--- backend/src/config/redis.js
+++ backend/src/config/redis.js
@@ -1,7 +1,23 @@
-const { createClient } = require("redis");
-const { CLIENT_KILL_FILTERS } = require("redis/dist");
+// const { createClient } = require("redis");
+// const { CLIENT_KILL_FILTERS } = require("redis/dist");
+//
+// const redis = createClient({ url: process.env.REDIS_URL });
+//
+// redis.on("error", (err) => console.error("Redis error:", err));
+//
+// (async () => await redis.connect())();
+// console.log("connected");
+// module.exports = redis;
 
-const redis = createClient({ url: process.env.REDIS_URL });
+const { createClient } = require("redis");
+
+const redis = createClient({
+  url: process.env.REDIS_URL,
+  socket: {
+    tls: true,
+    rejectUnauthorized: false,
+  },
+});
 
 redis.on("error", (err) => console.error("Redis error:", err));
 
