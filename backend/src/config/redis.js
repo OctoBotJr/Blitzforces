28b0ea7b6919a00311e4e196501a6d17fb5a15f2
OctoBotJr <vvsanjeevprasanna@gmail.com>
2026-05-28 21:12:26 +0530

    Update redis.js

--- backend/src/config/redis.js
+++ backend/src/config/redis.js
@@ -1,14 +1,3 @@
-// const { createClient } = require("redis");
-// const { CLIENT_KILL_FILTERS } = require("redis/dist");
-//
-// const redis = createClient({ url: process.env.REDIS_URL });
-//
-// redis.on("error", (err) => console.error("Redis error:", err));
-//
-// (async () => await redis.connect())();
-// console.log("connected");
-// module.exports = redis;
-
 const { createClient } = require("redis");
 
 const redis = createClient({
